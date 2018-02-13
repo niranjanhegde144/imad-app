59bd52048df8a468836cf1ffd04041f1a7dd738b
Niranjan Hegde <hegdeniranjan144@gmail.com>
2018-02-13 19:31:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ element.innerHTML = "Main html elements has been changed";
 
 var image = document.getElementById('madi');
 image.onclick = function() {
-  image.style.marginLeft = '150px';  
+    var interval = setInterval(moveRight, 50);
 };
 
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
