9ed17a47ff782631543c5d979c58571b58f1308c
Niranjan Hegde <hegdeniranjan144@gmail.com>
2018-02-13 14:48:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+  title: "article-one", 
+  heading : 'Article-one',
+  date: '13 feb 2018',
+  content: `
+  <p>Paragraphs begin for article one from here...</p>
+  <p>Paragraphs begin for article one from here...</p>
+  <p>Paragraphs begin for article one from here...</p>
+  <p>Paragraphs begin for article one from here...</p>
+  <p>Paragraphs begin for article one from here...</p>
+  <p>Paragraphs begin for article one from here...</p>
+  <p>Paragraphs begin for article one from here...</p>
+  `
+};
+
+
+function createTemplate(data) {
+ 
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlContent = 
+        //beginning of the string
+        `
+        <html>
+        <head>
+            <title>
+                ${title}.
+            </title>
+            <link  rel = "stylesheet" href = "/ui/style.css">
+            <meta name ="viewport" content="width=device-width, initial-scale = 1">
+        </head>
+        <body>
+            <div class = "container">
+                <div>
+                    <a href = '/'>Link to home page</a>
+                    <hr/>
+                </div>
+                <div>
+                    <h1>${heading}</h1>
+                </div>
+                <div>
+                ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `//end of string
+    return htmlContent;
+} 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function (req, res) {
-    res.sendfile(path.join(__dirname, 'ui', "article-one.html")); 
+    res.sendfile(createTemplate(articleOne)); 
 });
 
 app.get('/article-two', function(req, res) {
