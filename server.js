80eda18ff7ceb1a15ac3b696b9f20ba5ea1e0f1e
Niranjan Hegde <hegdeniranjan144@gmail.com>
2018-02-13 14:49:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req, res) {
-    res.sendfile(createTemplate(articleOne)); 
+    res.send(createTemplate(articleOne)); 
 });
 
 app.get('/article-two', function(req, res) {
