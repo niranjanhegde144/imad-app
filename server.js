bbdd25452cdccab69447afc63e23f1efc50133f5
Niranjan Hegde <hegdeniranjan144@gmail.com>
2018-02-13 14:58:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function (req, res) {
-    var articlleName = req.parans.articleName;
+    var articlleName = req.params.articleName;
     res.send(createTemplate(articles[articleName])); 
 });
 
