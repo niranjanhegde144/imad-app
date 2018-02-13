06f2c5b22e0e5f6063b61a78d7b812a841c468c9
Niranjan Hegde <hegdeniranjan144@gmail.com>
2018-02-13 19:22:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,3 @@
 console.log('Loaded!');
-alert("loading");
-var mainText = document.getElementById('main-text');
-mainText.innerHtml = "Main html elements has been changed"
+var element = document.getElementById('main-text');
+element.innerHTML = "Main html elements has been changed"
