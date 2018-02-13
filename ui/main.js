4d638ce924bcd8646067db4c58584dfe0ae67736
Niranjan Hegde <hegdeniranjan144@gmail.com>
2018-02-13 19:27:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "Main html elements has been changed";
+
+var image = document.getElementById('madi');
+image.onclick = function() {
+  image.style.marginLeft = '150px';  
+};
+
